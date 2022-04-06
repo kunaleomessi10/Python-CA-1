32bf6a1ed239fa063a20971d1b48c82682984a30
kunaleomessi10 <kunal.kini5@gmail.com>
2022-04-06 23:31:33 +0100

    adding employee class

--- python-project.py
+++ python-project.py
@@ -1,1 +1,13 @@
+class Employee:
+  employdata = {}
 
+  def __init__(self, StaffID, LastName, FirstName, RegHours, HourlyRate, OTMultiple, TaxCredit, StandardBand):
+    self.StaffID = StaffID
+    self.LastName = LastName
+    self.FirstName = FirstName
+    self.RegHours = RegHours
+    self.HourlyRate = HourlyRate
+    self.OTMultiple = OTMultiple
+    self.TaxCredit = TaxCredit
+    self.StandardBand = StandardBand
+    Employee.employdata[StaffID] = self
