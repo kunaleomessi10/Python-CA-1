623abe09d80ad3c891d249604c93100a22afe373
kunaleomessi10 <kunal.kini5@gmail.com>
2022-04-06 23:56:25 +0100

    changes

--- python-project.py
+++ python-project.py
@@ -1,13 +1,13 @@
 class Employee:
-  employdata = {}
+  edata = {}
 
-  def __init__(self, StaffID, LastName, FirstName, RegHours, HourlyRate, OTMultiple, TaxCredit, StandardBand):
-    self.StaffID = StaffID
-    self.LastName = LastName
-    self.FirstName = FirstName
-    self.RegHours = RegHours
-    self.HourlyRate = HourlyRate
-    self.OTMultiple = OTMultiple
-    self.TaxCredit = TaxCredit
-    self.StandardBand = StandardBand
-    Employee.employdata[StaffID] = self
+  def __init__(self, S_ID, Last_Name, First_Name, Reg_Hours, Hourly_Rate, OT_Multiple, Tax_Credit, Standard_Band):
+    self.StaffID = S_ID
+    self.LastName = Last_Name
+    self.FirstName = First_Name
+    self.RegHours = Reg_Hours
+    self.HourlyRate = Hourly_Rate
+    self.OTMultiple = OT_Multiple
+    self.TaxCredit = Tax_Credit
+    self.StandardBand = Standard_Band
+    Employee.edata[S_ID] = self
