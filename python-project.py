05ecabdca6331be2beea94740f670557b2f3a48e
kunaleomessi10 <kunal.kini5@gmail.com>
2022-04-08 02:03:25 +0100

    testing

--- python-project.py
+++ python-project.py
@@ -1,13 +1,17 @@
 class Employee:
-  edata = {}
 
-  def __init__(self, S_ID, Last_Name, First_Name, Reg_Hours, Hourly_Rate, OT_Multiple, Tax_Credit, Standard_Band):
-    self.StaffID = S_ID
-    self.LastName = Last_Name
-    self.FirstName = First_Name
-    self.RegHours = Reg_Hours
-    self.HourlyRate = Hourly_Rate
-    self.OTMultiple = OT_Multiple
-    self.TaxCredit = Tax_Credit
-    self.StandardBand = Standard_Band
-    Employee.edata[S_ID] = self
+    def __init__(self, S_ID, Last_Name, First_Name, Reg_Hours, Hourly_Rate, OT_Multiple, Tax_Credit, Standard_Band):
+        self.StaffID = S_ID
+        self.LastName = Last_Name
+        self.FirstName = First_Name
+        self.RegHours = Reg_Hours
+        self.HourlyRate = Hourly_Rate
+        self.OTMultiple = OT_Multiple
+        self.TaxCredit = Tax_Credit
+        self.StandardBand = Standard_Band
+
+    def ComputePayment(self, HoursWorked, date):
+        NetPay= {}
+        NetPay["name"] = self.FirstName + " " + self.LastName
+        NetPay["Date"] = date
+
